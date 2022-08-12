7defba545a9f6b5c4c7bff9043ee7f5717131121
juanp <juanp@DESKTOP-JATAE0K>
2022-08-12 00:24:48 -0700

    Add project files.

---
+++ Controllers/PaymentsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using MVCJoelGramajoPrueba2.Models;
+
+namespace MVCJoelGramajoPrueba2.Controllers
+{
+    public class PaymentsController : Controller
+    {
+        private PRODUCT_DBEntities db = new PRODUCT_DBEntities();
+
+        // GET: Payments
+        public ActionResult Index()
+        {
+            var payments = db.Payments.Include(p => p.Customer).Include(p => p.Product);
+            return View(payments.ToList());
+        }
+
+        // GET: Payments/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // GET: Payments/Create
+        public ActionResult Create()
+        {
+            ViewBag.CID = new SelectList(db.Customers, "consumerID", "firstname");
+            ViewBag.ProID = new SelectList(db.Products, "proID", "proName");
+            return View();
+        }
+
+        // POST: Payments/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "PID,CID,ProID,paymentType,amount")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Payments.Add(payment);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CID = new SelectList(db.Customers, "consumerID", "firstname", payment.CID);
+            ViewBag.ProID = new SelectList(db.Products, "proID", "proName", payment.ProID);
+            return View(payment);
+        }
+
+        // GET: Payments/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CID = new SelectList(db.Customers, "consumerID", "firstname", payment.CID);
+            ViewBag.ProID = new SelectList(db.Products, "proID", "proName", payment.ProID);
+            return View(payment);
+        }
+
+        // POST: Payments/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "PID,CID,ProID,paymentType,amount")] Payment payment)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(payment).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CID = new SelectList(db.Customers, "consumerID", "firstname", payment.CID);
+            ViewBag.ProID = new SelectList(db.Products, "proID", "proName", payment.ProID);
+            return View(payment);
+        }
+
+        // GET: Payments/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Payment payment = db.Payments.Find(id);
+            if (payment == null)
+            {
+                return HttpNotFound();
+            }
+            return View(payment);
+        }
+
+        // POST: Payments/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Payment payment = db.Payments.Find(id);
+            db.Payments.Remove(payment);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
